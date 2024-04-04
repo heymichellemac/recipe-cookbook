1805851af1f72ad674d445cf59b538a27118ffe2
heymichellemac <heymichellemac@gmail.com>
2024-04-04 20:32:03 +0100

    order recipes alphabetically

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,11 @@
 // .eleventy.js
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addCollection("recipes", function(collection) {
-        return collection.getFilteredByGlob("recipes/*.md");
+    eleventyConfig.addCollection("recipes", function(collectionApi) {
+        // Get all recipe posts
+        let recipes = collectionApi.getFilteredByGlob("recipes/*.md");
+        // Sort recipes alphabetically by title
+        recipes.sort((a, b) => (a.data.title > b.data.title) ? 1 : -1);
+        return recipes;
     });
 
     eleventyConfig.addPassthroughCopy({
